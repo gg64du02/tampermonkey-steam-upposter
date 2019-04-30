b3cf7c19aa68db5cc2b7b93efb9a22c2915bd65a
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-04-30 15:28:21 +0200

    Another script to upvote a server

---
+++ Upvoting HaiPing.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name        Upvoting HaiPing
+// @namespace   sdark
+// @description Upvoting HaiPing
+// @include   http*://arkservers.net/vote/172.107.170.199:27016/
+
+// @version   0.1
+// @grant     none
+// @require   http://code.jquery.com/jquery-2.1.1.min.js
+// ==/UserScript==
+
+
+/*
+you need to be logged in to up vote a server
+set the voting URL as a home page in your broswer
+and everytime you will open your broswer the upvoting will be done
+*/
+
+console.log('starting');
+
+var upvote_button = document.getElementsByClassName('btn btn-success');
+
+upvote_button[0].click();
+
+console.log('1st button;');
+
+document.getElementsByClassName('btn btn-success')[0].click()
+
+console.log('2nd button;');
+
+console.log('script finished');
+
